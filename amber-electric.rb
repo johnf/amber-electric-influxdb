3014a05f96d4867238f548816b97a641ecd7af2f
John Ferlito <johnf@inodes.org>
2020-01-01 15:57:33 +1100

    refactor

--- amber-electric.rb
+++ amber-electric.rb
@@ -12,14 +12,48 @@ class AmberElectric
   base_uri 'https://api-bff.amberelectric.com.au/api/v1.0'
   # debug_output
 
-  def initialize(username, password)
-    @username = username
-    @password = password
+  attr_reader :influxdb, :once
 
-    login
+  def initialize
+    @once = ENV['ONCE']
+
+    influx_hostname = ENV['INFLUXDB_HOSTNAME'] || abort('INFLUXDB_HOSTNAME not set')
+    influx_database = ENV['INFLUXDB_DATABASE'] || abort('INFLUXDB_DATABASE not set')
+    @influxdb = InfluxDB::Client.new(influx_database, host: influx_hostname)
+
+    username = ENV['AE_USERNAME'] || abort('AE_USERNAME not set')
+    password = ENV['AE_PASSWORD'] || abort('AE_PASSWORD not set')
+    login(username, password)
   end
 
-  def price_list
+  def run
+    price_list = fetch_price_list
+
+    loop do
+      data = [
+        {
+          series: 'live',
+          values: {
+            price: price_list['currentPriceKWH'],
+            renewables: price_list['currentRenewableInGrid'],
+            colour: price_list['currentPriceColor']
+          },
+          timestamp: Time.iso8601(price_list['currentPricePeriod'].sub(/Z$/, '')).to_i
+        },
+      ]
+
+      influxdb.write_points(data)
+
+      if once
+        ap usage
+        exit
+      end
+
+      sleep 300
+    end
+  end
+
+  def fetch_price_list
     options = {
     }
     result = self.class.post('/Price/GetPriceList', options).parsed_response
@@ -28,13 +62,22 @@ class AmberElectric
     result['data']
   end
 
+  def usage
+    options = {
+    }
+    result = self.class.post('/UsageHub/GetUsageForHub', options).parsed_response
+    raise 'Could not fetch usage' unless result['serviceResponseType'] == 1
+
+    result['data']
+  end
+
   private
 
-  def login
+  def login(username, password)
     options = {
       body: {
-        username: @username,
-        password: @password
+        username: username,
+        password: password
       }.to_json
     }
 
@@ -46,39 +89,9 @@ class AmberElectric
   end
 end
 
+AmberElectric.new.run
 # Gran data from environment
 #
-username = ENV['AE_USERNAME'] || abort('AE_USERNAME not set')
-password = ENV['AE_PASSWORD'] || abort('AE_PASSWORD not set')
-influx_hostname = ENV['INFLUXDB_HOSTNAME'] || abort('INFLUXDB_HOSTNAME not set')
-influx_database = ENV['INFLUXDB_DATABASE'] || abort('INFLUXDB_DATABASE not set')
-once = ENV['ONCE']
-
-ae = AmberElectric.new(username, password)
-influxdb = InfluxDB::Client.new(influx_database, host: influx_hostname)
-
-loop do
-  price_list = ae.price_list
-
-  data = [
-    {
-      series: 'live',
-      values: {
-        price: price_list['currentPriceKWH'],
-        renewables: price_list['currentRenewableInGrid'],
-        colour: price_list['currentPriceColor']
-      },
-      timestamp: Time.iso8601(price_list['currentPricePeriod'].sub(/Z$/, '')).to_i
-    },
-  ]
-
-  influxdb.write_points(data)
-
-  if once
-    ap data
-    ap usage
-    exit
-  end
-
-  sleep 300
-end
+# usage = ae.usage
+# usage['thisWeekDailyUsage'].each do |day|
+# end
